4140c7e2e905e1fd37dc9c1b80ab2c2a167b6e51
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-10-29 16:30:07 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  root "static_pages#home"
+  get '/help', to: "static_pages#help"
+  get '/about', to: "static_pages#about"
+  get '/contact', to: "static_pages#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,5 +10,4 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-   root "static_pages#home"
 end
