2463f34f2baefc2b7a6d0a9a4eb69bbda64b9ece
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-10-29 18:00:51 +0900

    users#newへのルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root "static_pages#home"
   get '/help',    to: "static_pages#help"
   get '/about',   to: "static_pages#about"
   get '/contact', to: "static_pages#contact"
+  get '/signup',  to: "users#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
