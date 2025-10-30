d5afcee2c91256092dedc4ad05a6f13b9f807a5f
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-10-30 15:10:22 +0900

    タイポ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   validates :email, presence: true, 
                     length: {maximum: 255},
                     format: {with: VALID_EMAIL_REGEX },
-                    uniquness: true 
+                    uniqueness: true 
 end
