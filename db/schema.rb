7676a42a48f11b01f7d0c90c554362214b9adc9c
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-10-30 15:54:00 +0900

    userモデルemailカラムにユニーク制約付与をマイグレーション後

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_30_014323) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_30_064014) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
