614f92965a1a9d0b0cf81d32e8045d665a3173ba
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-10-30 16:18:24 +0900

    email小文字化を破壊的メソッドに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save {self.email = email.downcase}
+  before_save {self.email.downcase!}
 
   validates :name,  presence: true, 
                     length: {maximum: 50}
