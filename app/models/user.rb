9671921aa7e5bb1a69589b03748ed87dcd801416
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-10-30 16:01:54 +0900

    emailを小文字に変換して一意性を保証するコードに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase}
+
   validates :name,  presence: true, 
                     length: {maximum: 50}
 
@@ -6,5 +8,5 @@ class User < ApplicationRecord
   validates :email, presence: true, 
                     length: {maximum: 255},
                     format: {with: VALID_EMAIL_REGEX },
-                    uniqueness: {case_sensitive: false} 
+                    uniqueness: true 
 end
