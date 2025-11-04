b9e530e060086644fa7e142c0563e8eb15c5105b
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-04 17:51:25 +0900

    is_legged_in?メソッドを追加

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,10 @@ module ActiveSupport
     fixtures :all
     include ApplicationHelper
 
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
+
     # Add more helper methods to be used by all tests here...
   end
 end
