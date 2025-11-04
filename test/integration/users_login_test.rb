38d68150f38c2f44684c0501e768e78025c40ede
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-04 17:34:44 +0900

    setupとテストケースの追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,6 +5,22 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+  def setup
+    @user = users(:michael)
+  end
+
+  test "メールアドレスが正しくて、パスワードが誤っている場合のテスト" do
+    get login_path
+    assert_template "sessions/new"
+    post login_path, params: { session: { email: @user.email,
+                                          password: "invalid" } }
+    assert_response :unprocessable_content
+    assert_template "sessions/new"
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template "sessions/new"
@@ -14,5 +30,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "login with valid information" do
+    post login_path, params: { session: { email: @user.email,
+                                          password: "password" } }
+
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template "users/show"
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
