01cd980bce4a072b69cd49524bfc78ea81ad4519
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-04 14:18:02 +0900

    current_userメソッドを追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,14 @@
 module SessionsHelper
+
+  #渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+  #現在ログイン中のユーザーを返す（いる場合）
+  def current_user
+    if session[:user_id]
+      @currrent_user ||= User.find_by(id: session[:user_id])
+    end
   end
 end
