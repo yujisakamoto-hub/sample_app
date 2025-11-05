dc3ee7c8b93fd8cfbd94203ff7173e7dcf250125
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-05 11:28:36 +0900

    rememberメソッドの定義/クラスメソッドの書き方変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save {self.email.downcase!}
 
   validates :name,  presence: true, 
@@ -15,14 +16,20 @@ class User < ApplicationRecord
                        length: {minimum: 8}
 
   #渡された文字列のハッシュ値を返す
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
   #ランダムなトークンを返す
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  #永続的セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
