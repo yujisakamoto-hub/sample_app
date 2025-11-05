8fe13bced52286bba1fe88f769f3278491db7334
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-05 14:48:21 +0900

    テスト名を日本語化/authenticated?に対するテストの追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,21 +12,21 @@ class UserTest < ActiveSupport::TestCase
                      password: "#{TEST_PASSWORD}", password_confirmation: "#{TEST_PASSWORD}")
   end
 
-  test "should be valid" do
+  test "ユーザーが有効かどうかのテスト" do
     assert @user.valid?
   end
 
-  test "name should be present" do
+  test "nameの文字数制限のテスト" do
     @user.name = "a" * 51
     assert_not @user.valid?
   end
 
-  test "email should be present" do
+  test "emailの文字数制限のテスト" do
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
 
-  test "email validation should accept valid addresses" do
+  test "有効なメールアドレスのテスト" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp 
                          alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
@@ -35,7 +35,7 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test "email validation should reject invalid addresses" do
+  test "無効なメールアドレスのテスト" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.foo@bar_baz.com
                            foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
@@ -44,26 +44,30 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test "email addresses should be unique" do
+  test "emailの一意性テスト" do
     duplicate_user = @user.dup
     @user.save
     assert_not duplicate_user.valid?
   end
 
-  test "email addresses should be saved as lowercase" do
+  test "emailが小文字で保存されているかのテスト" do
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
-  test "password should be present (nonblank)" do
+  test "passwordが空白だった場合のテスト" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
 
-  test "password should have a minimum length" do
+  test "passwordの文字数が足りない場合のテスト" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
+  end
+
+  test "ダイジェストが存在しない場合のauthenticated?のテスト" do
+    assert_not @user.authenticated?("")
   end
 end
