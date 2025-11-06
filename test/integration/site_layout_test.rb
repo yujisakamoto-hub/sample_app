5217a53de167014ac5e1e5abbf86e9aa944adc03
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-06 18:10:16 +0900

    リンクテストを追加

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,8 +4,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = users(:michael)
+  end
   
-  test "layout links" do
+  test "ログインしていない状態でのリンクテスト" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -13,6 +17,25 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    get contact_path
+    assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select "title", full_title("Sign up")
+  end
+
+  test "ログイン済みでのリンクテスト" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
