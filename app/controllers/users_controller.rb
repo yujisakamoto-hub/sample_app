d6453ed1a9f92e66230d3655d1d01c495b867266
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-06 13:46:29 +0900

    edit,updateアクションに対するbefore_actionを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -42,5 +43,15 @@ class UsersController < ApplicationController
                                    :password,
                                    :password_confirmation
                                   )
+    end
+
+    #beforeフィルタ
+
+    #ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください"
+        redirect_to login_path, status: :see_other
+      end
     end
 end
