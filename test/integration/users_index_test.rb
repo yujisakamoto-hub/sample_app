75a87172fca57c9145a67ca710039147f91dbd45
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-07 16:10:59 +0900

    users#indexの統合テスト完成

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -6,16 +6,32 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   # end
 
   def setup
-    @user = users(:michael)
+    @admin = users(:michael)
+    @non_admin = users(:archer)
   end
 
-  test "indexにpaginationが有効かどうか" do
-    log_in_as(@user)
+  test "管理者がindexにアクセスしたときのテスト" do
+    log_in_as(@admin)
     get users_path
     assert_template "users/index"
     assert_select "ul.pagination", count: 2
-    User.paginate(page: 1, per_page: 30).each do |user|
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
       assert_select "a[href=?]", user_path(user), text: user.name
+      unless user == @admin
+        assert_select "a[href=?]", user_path(user), text: "delete"
+      end
     end
+    assert_difference "User.count", -1 do
+      delete user_path(@non_admin)
+      assert_response :see_other
+      assert_redirected_to users_url
+    end
+  end
+
+  test "管理者以外がindexにアクセスしたときのテスト" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select "a", text: "delete", count: 0
   end
 end
