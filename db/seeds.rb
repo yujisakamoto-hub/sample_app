7dda957d7381591dd516cf0a6001ceb80cfc607f
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-07 17:59:46 +0900

    サンプルユーザーを有効化

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,9 @@ User.create!(name: "Example User",
              email: "example@railstutorial.org",
              password: "password",
              password_confirmation: "password",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 #追加のユーザーをまとめて生成する
 99.times do |n|
@@ -23,5 +25,7 @@ User.create!(name: "Example User",
   User.create!(name: name,
                email: email,
                password: password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
