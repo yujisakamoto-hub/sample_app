81e9f3a60c52e5a034764df59a8293c62dea679f
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-07 14:23:17 +0900

    admin属性をweb経由で編集できないことをテスト

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -44,4 +44,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_path
     assert_redirected_to login_url
   end
+
+  test "admin属性をweb経由で編集できない" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: { user: { password: "password",
+                                                    password_confirmation: "password",
+                                                    admin: true } }
+    assert_not @other_user.reload.admin?
+  end
 end
