0a90a9ec98941e959e28b68d324550b0ef3c5e26
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-10 17:15:18 +0900

    アカウント有効化機能に対する一連のテスト

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,33 +1,60 @@
 require "test_helper"
 
-class UsersIndexTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class UsersIndex < ActionDispatch::IntegrationTest
 
   def setup
     @admin = users(:michael)
     @non_admin = users(:archer)
   end
+end
 
-  test "管理者がindexにアクセスしたときのテスト" do
+class UsersIndexAdmin < UsersIndex
+
+  def setup
+    super
     log_in_as(@admin)
     get users_path
+  end
+end
+
+class UsersIndexAdminTest < UsersIndexAdmin
+
+  test "管理者はusers/indexにアクセスできる" do
     assert_template "users/index"
+  end
+
+  test "ページネーションが機能しているか" do
     assert_select "ul.pagination", count: 2
-    first_page_of_users = User.paginate(page: 1)
+  end
+
+  test "管理者にはdeleteリンクが表示される" do
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select "a[href=?]", user_path(user), text: user.name
       unless user == @admin
         assert_select "a[href=?]", user_path(user), text: "delete"
       end
     end
+  end
+
+  test "管理者はユーザーを削除できる" do
     assert_difference "User.count", -1 do
       delete user_path(@non_admin)
-      assert_response :see_other
-      assert_redirected_to users_url
+    end
+    assert_response :see_other
+    assert_redirected_to users_url
+  end
+
+  test "users/indexには有効化されたユーザーのみが表示される" do
+    User.paginate(page: 1).first.toggle!(:activated)
+    get users_path
+    assigns(:users).each do |user|
+      assert user.activated?
     end
   end
+end
+
+class UsersNonAdminIndexTest < UsersIndex
 
   test "管理者以外がindexにアクセスしたときのテスト" do
     log_in_as(@non_admin)
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,19 +1,21 @@
 require "test_helper"
 
-class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class UsersSignup < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+end
+
+class UsersSignupTest < UsersSignup
 
   test "ユーザー登録失敗パターンのテスト" do
     get signup_path
     assert_no_difference "User.count" do
       post users_path, params: { user: { name: "",
                                          email: "user@invalid",
-                                         password: "foo",
-                                         password_confirmation: "bar" 
-                                       }
-                               }
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
     end
     assert_response :unprocessable_content
     assert_template "users/new"
@@ -21,18 +23,52 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "div.alert.alert-danger"
   end
 
-  test "ユーザー登録のテスト" do
+  test "ユーザー登録及びアカウント有効化のテスト" do
     assert_difference "User.count", 1 do
       post users_path, params: { user: { name: "Example User",
                                          email: "user@example.com",
-                                         password: "password",
-                                         password_confirmation: "password"
-                                       }
-                               }
+                                         password:              "password",
+                                         password_confirmation: "password" } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+  end
+end
+
+class AccountAcitivationTest < UsersSignup
+
+  def setup
+    super
+    post users_path, params: { user: { name: "Example User",
+                                       email: "user@example.com",
+                                       password:              "password",
+                                       password_confirmation: "password" } }
+    @user = assigns(:user)
+  end
+
+  test "初期状態は有効化されていない" do
+    assert_not @user.activated?
+  end
+
+  test "アカウント有効化しないとログインできない" do
+    log_in_as(@user)
+    assert_not is_logged_in?
+  end
+
+  test "無効な有効化トークンではログインできない" do
+    get edit_account_activation_path("invalid token", email: @user.email)
+    assert_not is_logged_in?
+  end
+
+  test "無効なメールアドレスではログインできない" do
+    get edit_account_activation_path(@user.activation_token, email: "wrong")
+    assert_not is_logged_in?
+  end
+
+  test "有効なトークンとメールアドレスならログイン成功する" do
+    get edit_account_activation_path(@user.activation_token, email: @user.email)
+    assert @user.reload.activated?
     follow_redirect!
-#assert_template "users/show"
-    assert_not flash.blank?
-#assert is_logged_in?
+    assert_template "users/show"
+    assert is_logged_in?
   end
 end
