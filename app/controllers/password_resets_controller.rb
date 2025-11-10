b5c5d279080140ad8d9a015e0bb9a0d6b7e51ae5
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-10 18:36:33 +0900

    パスワード再設定用のcreateアクションを作成

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,19 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "パスワードリセット手順が記載されたメールを送信しました"
+      redirect_to root_url
+    else
+      flash[:danger] = "このメールアドレスは登録されていません"
+      render "new", status: :unprocessable_content
+    end
+  end
+
   def edit
   end
 end
