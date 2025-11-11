c908aaebfc85dc7f4aa9a23137d1307c3bb62d4a
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-11 19:17:28 +0900

    micropostに対するテストを作成

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+    @micropost = @user.microposts.build(content: "test")
+  end
+
+  test "有効かどうかチェック" do
+    assert @micropost.valid?
+  end
+
+  test "user_idがなければならない" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "contentは存在しなければならない" do
+    @micropost.content = "    "
+    assert_not @micropost.valid?
+  end
+
+  test "contentは140字以内でなければならない" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
+
+  test "最新の順に並べられているか" do
+    assert_equal microposts(:most_recent), Micropost.first
+  end
 end
