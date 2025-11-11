033b079c76aa09796f3eea42e07c83c5b30ff3df
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-11 12:15:47 +0900

    パスワード再設定コントローラー完成

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -22,9 +23,25 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "を入力してください")
+      render "edit", status: :unprocessable_content
+    elsif @user.update(user_params)
+      @user.forget
+      reset_session
+      log_in @user
+      flash[:success] = "パスワード再設定が完了しました"
+      redirect_to @user
+    else
+      render "edit", status: :unprocessable_content
+    end
   end
 
   private #---------------------------------------------------------------------------
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
 
     def get_user
       @user = User.find_by(email: params[:email])
@@ -34,6 +51,13 @@ class PasswordResetsController < ApplicationController
       unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
         flash[:danger] = "無効なユーザーです"
         redirect_to root_url
+      end
+    end
+
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "パスワード再設定期限が切れました"
+        redirect_to new_password_reset_url
       end
     end
 end
