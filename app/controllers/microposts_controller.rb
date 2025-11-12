d545e8a8ec65a38970cb596d20482833a58b748a
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-12 17:07:22 +0900

    削除対象のマイクロポストを検索するbeforeフィルターを定義

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -20,4 +21,9 @@ private
 
 def micropost_params
   params.require(:micropost).permit(:content)
+end
+
+def correct_user
+  @micropost = current_user.microposts.find_by(id: params[:id])
+  redirect_to root_url, status: :see_other if @micropost.nil?
 end
