145432bedb0e89410ecff60295ae57eba092ed09
坂本勇次 <yuji-sakamoto@MacBook-Pro.local>
2025-11-12 15:08:10 +0900

    createアクション作成まで

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,22 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "投稿しました"
+      redirect_to root_url
+    else
+      render "static_pages/home", status: :unprocessable_content
+    end
+  end
+
+  def destroy
+  end
+end
+
+private
+
+def micropost_params
+  params.require(:micropost).permit(:content)
 end
